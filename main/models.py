bb1900001fc799083ee3a784edc86e593cff9d3b
MilesNaismith <truskov@inbox.ru>
2018-10-30 17:37:17 +0300

    first commit

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    img = models.ImageField(blank=True, upload_to='images/')
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    
+
---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+# Create your views here.
+def home(request):
+    return render(request, 'main/home.html')
+
+def portfolio(request):
+    posts = Post.objects.filter().order_by('-created_date')
+    return render(request, 'main/portfolio.html', {'posts':posts})
